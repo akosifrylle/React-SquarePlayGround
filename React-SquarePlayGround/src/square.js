a3cd7511464ffc6f86a78caca4e0adb390f347d3
Frylle Bancaso <fryllebancaso@cloudcommercepro.com>
2020-02-09 22:36:08 +0000

    - Initial commit.

---
+++ React-SquarePlayGround/src/square.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Square.css'
+
+export default class Square extends React.Component {
+    constructor() {
+        //super(props);
+        super();
+    }
+
+    render() {
+        const style = {
+            width: '100px',
+            height: '100px',
+            backgroundColor: this.props.initialColor
+        };
+
+        return (
+                <div style={style} className="Square">
+                    <div className="SquareControls">
+                    <input ref={(elem) => { console.log(elem.value) }} type="text" placeholder="Color..." />
+                    <button>Ok</button>
+                    </div>
+                </div>
+        );
+    }
+
+}
